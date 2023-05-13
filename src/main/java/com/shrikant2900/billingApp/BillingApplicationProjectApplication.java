d1874270c425e136e82a3d1c1dd1b87d134942dc
Dere <shrikant-moreshwar.dere@capgemini.com>
2023-05-13 12:17:57 +0530

    first commit

---
+++ src/main/java/com/shrikant2900/billingApp/BillingApplicationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shrikant2900.billingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingApplicationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingApplicationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrikant2900/billingApp/BillingApplicationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrikant2900.billingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingApplicationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
